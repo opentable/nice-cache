eb9bacd55143b8ef9ea61e4cdd313cfee6969be4
Matteo Figus <matteofigus@gmail.com>
2014-04-29 17:15:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+nice-cache
+=============
+
+A nice in-memory cache for node.js. In addiction of the classic auto-flush mechanism, it comes with an auto-refresh mechanism.
+
+Node version: **0.8.0** required
+
+Build status: [![Build Status](https://secure.travis-ci.org/matteofigus/nice-cache.png?branch=master)](http://travis-ci.org/matteofigus/nice-cache)
+
+[![NPM](https://nodei.co/npm/nice-cache.png?downloads=true)](https://npmjs.org/package/nice-cache)
+
+# Installation
+
+```shell
+npm install nice-cache
+```
+
+# Usage
+
+A simple usage with a web-server that consumes an API or reads from a DB:
+
+* Try getting a value from the cache
+* When a key is not found:
+   * you set the value
+   * then you subscribe a delegate function to the refresh loop, that will happen every 60 seconds.
+
+```js
+var Cache = require('nice-cache'),
+    http = require('http');
+
+var cache = new Cache({ refreshInterval: 60 });
+
+http.createServer(function (req, res) {
+
+  var getUsers = function(callback){
+    // do something asynchronous...
+    callback(err, result);
+  };
+
+  var users = cache.get('api-responses', 'users');
+
+  if(!!users)
+    return res.end(users);
+
+  getUsers(function(err, result){
+    cache.set('api-responses', 'users', result);
+    cache.sub('api-responses', 'users', getUsers);
+    res.end(result);
+  });
+
+}).listen(1337, '127.0.0.1');
+```
+
+# API
+
+### new Cache([options])
+
+Gets the current instance of the cache (eventually after creating it).
+
+The optional `options` object can consist on one of more of the following properties:
+* `singleton`, bool, default `true`, when false returns a new instance after any initialisation.
+* `refreshInterval`, int, default `0`, sets the refresh interval for the subscribed functions (seconds). When 0 it does nothing.
+* `flushInterval`, int, default `0`, sets the flush interval for all the keys. When 0 it never flushes.
+* `verbose`, bool, default `false`, writes stuff on the console for debugging purposes.
+
+### Cache#set([type, ] key, value)
+
+Sets a value. Type is optional.
+
+### Cache#get([type, ] key)
+
+Gets a value. Type is optional.
+
+### Cache#flushAll()
+
+Cleans the cache
+
+### Cache#sub([type, ] key, func)
+
+Subscribes the key-value assignment (paired with an optional type) to the refresh loop. The asynchronous function will need to have an (err, res) callback.
+
+# Tests
+
+```shell
+npm test
+```
+
+# License
+
+MIT
+
+# Contributors
+
+* [@matteofigus](https://github.com/matteofigus)
---
+++ test/nice-cache.js
@@ -0,0 +1,143 @@
+var should = require('should');
+var niceCache = require('../');
+
+describe('niceCache.set', function(){
+
+  var cache;
+
+  before(function(done){
+    cache = new niceCache({
+      singleton: false
+    });
+    cache.flush();
+    done();
+  });
+
+  it('should retrieve a value for a specific key', function(done){
+    cache.set('key', 'value');
+    cache.get('key').should.be.eql('value');
+    done();
+  });
+
+  it('should retrieve a value for a specific key and type', function(done){
+    cache.set('type', 'key', 'someValue');
+    cache.get('type', 'key').should.be.eql('someValue');
+    done();
+  });
+
+  it('should override a value when multiple sets are performed', function(done){
+    cache.set('name', 'John');
+    cache.set('name', 'Pier');
+    cache.get('name').should.be.eql('Pier');
+    done();
+  });
+});
+
+describe('niceCache.flush', function(){
+  it('should flush the cache', function(done){
+    var cache = new niceCache();
+
+    cache.set('a', 'b');
+    cache.set('b', 'c');
+
+    cache.flush();
+
+    var aIsUndefined = (typeof(cache.get('a')) === 'undefined'),
+        bIsUndefined = (typeof(cache.get('b')) === 'undefined');
+
+    aIsUndefined.should.be.eql(true);
+    bIsUndefined.should.be.eql(true);
+    done();
+  });
+});
+
+describe('niceCache.get', function(){
+  it('should get an undefined in case of a not existing key', function(done){
+    var cache = new niceCache(),
+        value = cache.get('notExisting');
+
+    var valueIsUndefined = (typeof(value) === 'undefined');
+
+    valueIsUndefined.should.be.eql(true);
+    done();
+  });
+
+  it('should get an undefined in case of an expired key', function(done){
+    var cache = new niceCache({
+      flushInterval: 0.005,
+      singleton: false
+    });
+
+    cache.set('key', 'value');
+
+    setTimeout(function(){
+      var valueIsUndefined = (typeof(cache.get('key')) === 'undefined');
+
+      valueIsUndefined.should.be.eql(true);
+      done();
+    }, 10);
+  });
+});
+
+describe('niceCache.sub', function(){
+  it('should allow a key to be refreshed in case of a refreshInterval option', function(done){
+    var cache = new niceCache({
+      refreshInterval: 0.01,
+      singleton: false
+    });
+
+    cache.set('key', 12345);
+
+    var c = 0;
+
+    var updateKey = function(callback){
+      c++;
+      callback(null, c);
+    };
+
+    cache.sub('key', updateKey);
+
+    cache.get('key').should.be.eql(12345);
+    setTimeout(function(){
+      cache.get('key').should.be.eql(1);
+      setTimeout(function(){
+        cache.get('key').should.be.eql(2);
+        done();
+      }, 10);
+    }, 11);
+  });
+
+  it('shouhldn\'t update a key when the subscribed delegate returns an error', function(done){
+    var cache = new niceCache({
+      refreshInterval: 0.005,
+      singleton: false
+    });
+
+    cache.set('type', 'key', 111);
+    cache.sub('type', 'key', function(callback){
+      callback('An error', null);
+    });
+    setTimeout(function(){
+      cache.get('type', 'key').should.be.eql(111);
+      done();
+    }, 0.01);
+  });
+});
+
+describe('niceCache', function(){
+  it('should be a singleton when singleton = false', function(done){
+    var cache1 = new niceCache({
+      singleton: true
+    });
+
+    cache1.flush();
+    cache1.set('key', '0000');
+
+    var cache2 = new niceCache({
+      singleton: true
+    });
+
+    cache2.get('key').should.be.eql('0000');
+    done();
+  });
+});
